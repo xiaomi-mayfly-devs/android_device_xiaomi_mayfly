f2a2a11f5210bf206ee9fe2f1fd38066e5aafc60
Michael Bestas <mkbestas@lineageos.org>
2022-10-10 22:37:47 +0300

    mayfly: Opt out of EPPE
    Change-Id: I71f54b1ad6de9d1406a3a55239acff1f4e426158

--- lineage_mayfly.mk
+++ lineage_mayfly.mk
@@ -9,6 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from mayfly device
