8bcbdedf338fa63f7babf5a5f3a5c7479f2103d1
Arian <arian.kulmer@web.de>
2023-06-25 14:43:55 +0200

    mayfly: Import audio configs from V14.0.10.0.TLTCNXM
    Change-Id: I32c54ebd2f55ed06bc49da567d6654f056909f50

--- device.mk
+++ device.mk
@@ -7,6 +7,11 @@
 # Inherit from sm8450-common
 $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/mixer_paths_overlay_static.xml \
+    $(LOCAL_PATH)/audio/resourcemanager_waipio_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/resourcemanager_waipio_mtp.xml
+
 # Overlay
 PRODUCT_PACKAGES += \
     FrameworksResMayfly \
