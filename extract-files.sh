0c50927308d3d7f11f1e6a86d3aabfef676a7687
Arian <arian.kulmer@web.de>
2023-11-15 18:55:16 +0100

    mayfly: Fixup camera blobs
    Change-Id: Ia0eff58ddecd3de7dac78f344b4a0f74bdd88f46

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/etc/camera/mayfly_enhance_motiontuning.xml|vendor/etc/camera/mayfly_motiontuning.xml)
+            sed -i 's/xml=version/xml version/g' "${2}"
+            ;;
+        vendor/etc/camera/pureView_parameter.xml)
+            sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
