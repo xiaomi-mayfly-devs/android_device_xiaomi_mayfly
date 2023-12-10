5828d908e70403509316b692f6940430f2020d7f
FlowerSea0208 <zzp060208@gmail.com>
2023-12-10 19:26:50 +0800

    tmp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,5 +15,13 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# Kernel modules
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_boot))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
+
 # Include proprietary files
 include vendor/xiaomi/mayfly/BoardConfigVendor.mk
