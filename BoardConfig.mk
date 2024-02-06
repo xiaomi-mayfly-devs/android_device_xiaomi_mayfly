e339285f2c265d9c2bfde1ca9e3f18b6de0674b4
FlowerSea0208 <zzp060208@gmail.com>
2024-02-06 11:13:28 +0800

    mayfly: Define device specific kernel modules
    Change-Id: I60122a4ed3ed8f04d03aca8fa87c97668544004a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,14 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# Kernel modules
+BOOT_KERNEL_MODULES += \
+    qcom_pm8008-regulator.ko \
+    fts_touch_spi.ko \
+    qcom-hv-haptics.ko
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(BOOT_KERNEL_MODULES)
+BOARD_VENDOR_KERNEL_MODULES_LOAD += $(BOOT_KERNEL_MODULES)
+
 # Vibrator
 TARGET_QTI_VIBRATOR_EFFECT_LIB := libqtivibratoreffect.xiaomi
 TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM := true
