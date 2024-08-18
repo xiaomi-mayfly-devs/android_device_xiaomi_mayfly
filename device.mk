1914955331f7820145042fb0a86771f9cb3e5231
FlowerSea0208 <zzp060208@gmail.com>
2024-08-19 03:23:06 +0800

    mayfly: Switch to source-built fxtunnel interface
    Change-Id: Ica88646503e5b2319a66f39a5e84eb6ea0e9092c

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 # Inherit from sm8450-common
 $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.fx.tunnel@1.0.vendor
+
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResMayfly \
